2ffe96e96c82a163bd29c01236cae69db195a5a4
sarah-sas <58082028+sarah-sas@users.noreply.github.com>
2019-12-30 11:14:45 -0800

    Add files via upload

--- commands/bal.js
+++ commands/bal.js
@@ -1,35 +1,34 @@
-const Discord = require("discord.js");
-const db = require("quick.db");
+const db = require('quick.db')
+const Discord = require('discord.js')
 
-module.exports.run = async(bot, message, args, utils) => {
+exports.run = async (client, message, args, config) => {
 
-    let user = message.mentions.members.first() || message.author;
-    //Get balance from database, and if the value is 0 (null) then send 0
-    //food
+
+    let user = message.mentions.members.first()
+
     let food = db.fetch(`food_${message.guild.id}_${user.id}`)
-    if (food === null) food = 0;
-    //parts
-    let parts = await db.fetch(`parts_${message.guild.id}_${user.id}`)
-    if (parts === null) parts = 0;
-    //Electricity
-    let electricity = await db.fetch(`electricity_${message.guild.id}_${user.id}`)
-    if (electricity === null) electricity = 0;
-    //Gas
-    let gas = await db.fetch(`gas_${message.guild.id}_${user.id}`)
-    if (gas === null) gas = 0;
-    //Cash
-    let cash = await db.fetch(`cash_${message.guild.id}_${user.id}`)
-    if (cash === null) cash = 0;
+    if (food === null) food = 0; // fetch mentioned users balance
 
-    //The part that actually sends the message with the balance
-    //gets the values from above witch is either 0 or the ballance that is already in the database
-    let foodEmbed = new Discord.RichEmbed()
-        .setColor("#s1fa818") //sets the color of the bar on the left side
-        .setDescription(`**${user}'s Balance**\n\nFood: ${food}k \nparts: ${parts}k \nparts: ${electricity}k \nparts: ${gas}k \nparts: ${cash}k`); //the actual message that displays the values
-    message.channel.send(foodEmbed) //send the message
-};
+    let parts = db.fetch(`parts_${message.guild.id}_${user.id}`)
+    if (parts === null) parts = 0; // fetch mentioned users balance
+
+    let electricity = db.fetch(`electricity_${message.guild.id}_${user.id}`)
+    if (electricity === null) electricity = 0; // fetch mentioned users balance
+
+    let gas = db.fetch(`gas_${message.guild.id}_${user.id}`)
+    if (gas === null) gas = 0; // fetch mentioned users balance
+
+    let cash = db.fetch(`cash_${message.guild.id}_${user.id}`)
+    if (cash === null) cash = 0; // fetch mentioned users balance
+
+    let embed = new Discord.RichEmbed()
+    .setDescription(`${user} has**\n\nFood: ${food}k \nParts: ${parts}k \nElectricity: ${electricity}k \nGas: ${gas}k \nCash: ${cash}k \n\nhere you go${message.author}`)
+    .setColor("GREEN")
+    .setTimestamp()
+    message.channel.send(embed)
+}
 
 module.exports.help = {
-    name: "balance", //the command name(the thing to use the command)
+    name: "bal", //the command name(the thing to use the command)
     aliases: ["bal"] //the shorter version to use the command example: "prefix" bal
 }
