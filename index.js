f9b4335da963030d3894f2f77c9f3939808237cc
sarah-sas <58082028+sarah-sas@users.noreply.github.com>
2020-01-30 00:07:15 -0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,60 +1,74 @@
-const botconfig = require("./botconfig.json");
-const Discord = require("discord.js");
-const fs = require("fs");
+const fs = require('fs');
+const Discord = require('discord.js');
+const { prefix, token } = require('./config.json');
+
 const client = new Discord.Client();
+client.commands = new Discord.Collection();
 
-bot.commands = new Discord.Collection();
-bot.aliases = new Discord.Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.name, command);
+}
 
-fs.readdir("./commands/", (err, files) => {
+const cooldowns = new Discord.Collection();
 
-    if (err) console.log(err);
-    let jsfile = files.filter(f => f.split(".").pop() === "js");
-    if (jsfile.length <= 0) {
-        console.log("Couldn't find commands.");
-        return;
-    }
+client.once('ready', () => {
+	console.log('Ready!');
+});
 
+client.on('message', message => {
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-    jsfile.forEach((f, i) => {
-        let props = require(`./commands/${f}`);
-        console.log(`${f} loaded!`);
-        bot.commands.set(props.help.name, props);
-        props.help.aliases.forEach(alias => {
-            bot.aliases.set(alias, props.help.name);
+	const args = message.content.slice(prefix.length).split(/ +/);
+	const commandName = args.shift().toLowerCase();
 
-        });
-    });
-})
-bot.on("ready", async() => {
-    console.log(`${bot.user.username} is online on ${bot.guilds.size} servers!`);
-    bot.user.setActivity(`In Development`);
-    bot.user.setStatus('online');
+	const command = client.commands.get(commandName)
+		|| client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
 
-    bot.on("message", async message => {
-        if (message.author.bot) return;
-        if (message.channel.type === "dm") return;
-        let prefix = botconfig.prefix
-        let messageArray = message.content.split(" ");
-        let args = message.content.slice(prefix.length).trim().split(/ +/g);
-        let cmd = args.shift().toLowerCase();
-        let commandfile;
+	if (!command) return;
 
-        if (bot.commands.has(cmd)) {
-            commandfile = bot.commands.get(cmd);
-        } else if (bot.aliases.has(cmd)) {
-            commandfile = bot.commands.get(bot.aliases.get(cmd));
-        }
+	if (command.guildOnly && message.channel.type !== 'text') {
+		return message.reply('I can\'t execute that command inside DMs!');
+	}
 
-        if (!message.content.startsWith(prefix)) return;
+	if (command.args && !args.length) {
+		let reply = `You didn't provide any arguments, ${message.author}!`;
 
+		if (command.usage) {
+			reply += `\nThe proper usage would be: \`${command.usage}\``;
+		}
 
-        try {
-            commandfile.run(bot, message, args);
+		return message.channel.send(reply);
+	}
 
-        } catch (e) {}
-    })
-})
+	if (!cooldowns.has(command.name)) {
+		cooldowns.set(command.name, new Discord.Collection());
+	}
 
-client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secrets
+	const now = Date.now();
+	const timestamps = cooldowns.get(command.name);
+	const cooldownAmount = (command.cooldown || 3) * 1000;
+
+	if (timestamps.has(message.author.id)) {
+		const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
+
+		if (now < expirationTime) {
+			const timeLeft = (expirationTime - now) / 1000;
+			return message.reply(`please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command.name}\` command.`);
+		}
+	}
+
+	timestamps.set(message.author.id, now);
+	setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
+
+	try {
+		command.execute(message, args);
+	} catch (error) {
+		console.error(error);
+		message.reply('there was an error trying to execute that command!');
+	}
+});
+
+client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
